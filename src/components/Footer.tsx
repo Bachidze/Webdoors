5fb23946f52988412e060f1c84182eb63223ae68
bachidze <bachidze90@gmail.com>
2024-02-06 18:23:09 +0400

    Feat(Add):Responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,23 @@ const Footer = () => {
     <div>
         <div className="flex justify-center mt-[150px] flex-col items-center">
             <div>
-                <img src="/assets/Webdoors.svg" alt="WebdDoorMainImg" />
+                <img className='md:w-[252px] md:mb-[35px]' src="/assets/Webdoors.svg" alt="WebdDoorMainImg" />
             </div>
-            <div className="flex gap-4 pt-5">
-                <img src="/assets/BE.svg" alt="BE" />
-                <img src="/assets/Instagram.svg" alt="Insta" />
-                <img src="/assets/twitter (1).svg" alt="Twitter" />
-                <img src="/assets/Llinkd.svg" alt="Linkdein" />
-                <img src="/assets/Faceb.svg" alt="Facebook" />
-                <img src="/assets/youtube.svg" alt="YT" />
-                <img src="/assets/dribble.svg" alt="BasketBall" />
-                <img src="/assets/Ggithub.svg" alt="Git" />
+            <div className="flex gap-4 pt-5 md:gap-[25px]">
+                <img className='md:w-[70px]' src="/assets/BE.svg" alt="BE" />
+                <img className='md:w-[70px]' src="/assets/Instagram.svg" alt="Insta" />
+                <img className='md:w-[70px]' src="/assets/twitter (1).svg" alt="Twitter" />
+                <img className='md:w-[70px]' src="/assets/Llinkd.svg" alt="Linkdein" />
+                <img className='md:w-[70px]' src="/assets/Faceb.svg" alt="Facebook" />
+                <img className='md:w-[70px]' src="/assets/youtube.svg" alt="YT" />
+                <img className='md:w-[70px]' src="/assets/dribble.svg" alt="BasketBall" />
+                <img className='md:w-[70px]' src="/assets/Ggithub.svg" alt="Git" />
             </div>
-            <div className="flex items-center mt-2 relative left-[10px]">
+            <div className="flex items-center mt-2 relative left-[10px] md:left-[92px] md:mt-[30px]">
                     <div className="first">
 
                     </div>
-                    <div className="second">
+                    <div className="second md:w-[520px]">
 
                     </div>
                     <div className="third">
@@ -33,13 +33,13 @@ const Footer = () => {
 
             </div>
         </div>
-        <div className='ml-4 flex flex-col items-center'>
-            <div className='flex gap-4 mt-4 relative right-[78px]'>
+        <div className='ml-4 flex flex-col items-center md:flex-row justify-center md:mt-[30px]'>
+            <div className='flex gap-4 mt-4 relative right-[78px] md:left-[405px]'>
                 <img src="/assets/Compliant.svg" alt="CompliantImg" />
                 <img src="/assets/EBA.svg" alt="EBAImg" />
             </div>
-            <div className='mt-4 mb-4 relative right-[30px]'>
-                <h4 className='text-[#98A5B2] text-16px font-normal'>Copyright © 2018-2022 all rights reserved</h4>
+            <div className='mt-4 mb-4 relative right-[30px] md:right-[330px] top-[10px]'>
+                <h4 className='text-[#98A5B2] text-[16px] font-normal'>Copyright © 2018-2022 all rights reserved</h4>
             </div>
         </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,18 @@
 const Header = () => {
   return (
     <div>
-        <div className="m-[15px] flex justify-between">
-            <img src="/assets/User.svg" alt="HeaderUserImg" />
-            <img src="/assets/MiddleIcon.svg" alt="MainIconMiddle" />
-            <img src="/assets/HeaderRight.svg" alt="HeaderRightIcon" />
+        <div className="m-[15px] flex justify-between  md:m-[30px] xl:mt-[77px]">
+        <div className="hidden xl:flex text-[white] gap-[20px] text-[18px] font-bold xl: relative top-[10px] ml-[180px]">
+          <a href="#">Services</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+        </div>
+            <img  className="xl:hidden " src="/assets/User.svg" alt="HeaderUserImg" />
+            <img className="md:w-[34px] xl:w-[39px] xl:relative right-[70px]" src="/assets/MiddleIcon.svg" alt="MainIconMiddle" />
+            <div className="flex gap-5">
+            <img className="hidden xl:flex" src="/assets/ManImg.svg" alt="" />
+            <img className="xl:mr-[180px]" src="/assets/HeaderRight.svg" alt="HeaderRightIcon" />
+            </div>
         </div>
     </div>
   )
