c7a547854272a97461f0e1293a775025086e7e8c
bachidze <bachidze90@gmail.com>
2024-02-08 09:55:34 +0400

    Feat(Remove):All

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,7 @@
 const Header = () => {
   return (
     <div>
-        <div className="m-[15px] flex justify-between  md:m-[30px] xl:mt-[77px]">
-        <div className="hidden xl:flex text-[white] gap-[20px] text-[18px] font-bold xl: relative top-[10px] ml-[180px]">
-          <a href="#">Services</a>
-          <a href="#">Projects</a>
-          <a href="#">About</a>
-        </div>
-            <img  className="xl:hidden " src="/assets/User.svg" alt="HeaderUserImg" />
-            <img className="md:w-[34px] xl:w-[39px] xl:relative right-[50px]" src="/assets/MiddleIcon.svg" alt="MainIconMiddle" />
-            <div className="flex gap-5">
-            <img className="hidden xl:flex" src="/assets/ManImg.svg" alt="" />
-            <img className="xl:mr-[180px]" src="/assets/HeaderRight.svg" alt="HeaderRightIcon" />
-            </div>
-        </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem expedita aliquam temporibus perferendis corrupti pariatur sequi eveniet quod, sint fugit!
     </div>
   )
 }
